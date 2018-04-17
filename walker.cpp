7ecf19c80a339b1225a20d4a22b448b4d814a44c
Brian <bmchristensen@gmail.com>
2018-04-16 21:00:16 -0700

    Implement random radians for direction vector (0 to 2pi).

--- walker.cpp
+++ walker.cpp
@@ -1,15 +1,14 @@
 #include "Walker.h"
 
 Walker::Walker() {
-	//this->walkerPosition;
-	//this->walkerDirection;
+	this->walkerPosition;
+	this->walkerDirection;
 	this->size = 10;
 	this->color = ofColor((int)ofRandom(0, 255), (int)ofRandom(0, 255), (int)ofRandom(0, 255));
 }
 
 void Walker::move() {
-	//walkerDirection.set(ofRandom(0, 2 * PI), ofRandom(0, 2 * PI));
-	walkerDirection.set(ofRandom(-360, 360), ofRandom(-360, 360));
+	walkerDirection.set(ofDegToRad(ofRandom(-360, 360)), ofDegToRad(ofRandom(-360, 360)));
 	walkerPosition += walkerDirection.normalize();
 }
 
