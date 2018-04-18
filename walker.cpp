1d4dad5400e2e7006f231766c076dc073935d6b6
Brian <bmchristensen@gmail.com>
2018-04-17 20:10:07 -0700

    Correct formatting. Adjust noise timing.

--- walker.cpp
+++ walker.cpp
@@ -1,20 +1,21 @@
-#include "Walker.h"
+#include "walker.h"
 
 Walker::Walker() {
-	this->walkerPosition;
-	this->walkerDirection;
+	//this->walkerPosition;
+	//this->walkerDirection;
 	this->size = 10;
-	this->color = ofColor((int)ofRandom(0, 255), (int)ofRandom(0, 255), (int)ofRandom(0, 255));
+	this->color = ofColor(static_cast<int>(ofRandom(0, 255)), (int)ofRandom(0, 255), (int)ofRandom(0, 255));
 	this->timeX = ofRandom(0, 10000);
 	this->timeY = ofRandom(0, 10000);
 }
 
 void Walker::move() {
-	walkerDirection.set(ofDegToRad(ofMap(ofNoise(timeX), 0, 1, -360, 360)), ofDegToRad(ofMap(ofNoise(timeY), 0, 1, -360, 360)));
+	walkerDirection.set(ofDegToRad(ofMap(ofNoise(timeX), 0, 1, -360, 360)),
+	                    ofDegToRad(ofMap(ofNoise(timeY), 0, 1, -360, 360)));
 	walkerPosition += walkerDirection.normalize();
 
-	timeX += 0.005;
-	timeY += 0.005;
+	timeX += 0.05;
+	timeY += 0.05;
 }
 
 void Walker::draw() {
