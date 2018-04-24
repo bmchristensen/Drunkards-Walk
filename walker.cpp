6c7b81cb63da03c5acdc1d0d8fbb8a05c03753ce
Brian <bmchristensen@gmail.com>
2018-04-23 17:26:39 -0700

    Remove two-dimmensional perlin noise implementation. Utilize ofRotate, unit vector for Perlin Noise implementation of Brownian Motion.

--- walker.cpp
+++ walker.cpp
@@ -2,22 +2,19 @@
 
 Walker::Walker() {
 	this->walkerPosition = ofVec2f(0, 0);
-	this->walkerDirection = ofVec2f(1, 1);
+	this->walkerDirection = ofVec2f(ofRandom(-1, 1), ofRandom(-1, 1));
+	this->walkerDirection.normalize();
 	this->size = 10;
 	this->color = ofColor(static_cast<int>(ofRandom(0, 255)), static_cast<int>(ofRandom(0, 255)),
 	                      static_cast<int>(ofRandom(0, 255)));
 
-	this->time = ofRandom(0, 100);
-	this->timeY = ofRandom(0, 100);
+	this->time = ofRandom(0, 10000);
 }
 
 void Walker::move() {
-	walkerDirection.set(ofMap(ofNoise(time), 0, 1, -180, 180), ofMap(ofNoise(timeY), 0, 1, -180, 180));
-	//walkerDirection.rotate(ofMap(ofNoise(time),0, 1, -180, 180));
-	walkerPosition += walkerDirection.normalize();
-
-	time += 0.008;
-	timeY += 0.008;
+	walkerDirection.rotate(ofMap(ofNoise(time), 0, 1, -10, 10));
+	walkerPosition += walkerDirection;
+	time += 0.001;
 }
 
 void Walker::draw() {
